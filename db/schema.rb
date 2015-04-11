2a9f9b4bac2c602633b8cd4a06ef2a05e17abf71
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-11 15:27:16 +0200

    add deck

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "decks", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
