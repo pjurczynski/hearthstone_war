efed0c84a916641ad528aca89a6769c9fc3896f8
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-12 16:58:33 +0200

    add more specs for playing a card by a player

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,6 +5,12 @@ class Player
   validates_presence_of :name, :available_cards
 
   def play_card
+    return self.played_card unless has_cards?
+    return self.played_card unless played_card.dead?
     self.played_card = available_cards.pop
+  end
+
+  def has_cards?
+    available_cards.any? || not(played_card.dead?)
   end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -12,6 +12,41 @@ describe Player do
     describe "#play_card" do
       it { expect{ subject.play_card }.to change{ player.played_card } }
       it { expect{ subject.play_card }.to change{ player.available_cards.count }.by(-1) }
+
+      context "already played a card" do
+        before { subject.play_card }
+
+        it "doesn't draw a new card" do
+          expect{ subject.play_card }.not_to change{ player.available_cards.count }
+          expect{ subject.play_card }.not_to change{ player.played_card }
+        end
+      end
+
+      context "has no more cards" do
+        before { subject.available_cards = [] }
+
+        it "plays with dead cards" do
+          subject.play_card
+          expect(subject.played_card.dead?).to be_truthy
+        end
+      end
+    end
+
+    describe "#has_cards?" do
+      context "has some available cards" do
+        it { expect(subject.has_cards?).to be_truthy }
+      end
+
+      context "have no cards left" do
+        before { subject.available_cards = [] }
+
+        it { expect(subject.has_cards?).to be_falsey }
+
+        context "has a card on hand" do
+          before { subject.played_card = build(:card) }
+          it { expect(subject.has_cards?).to be_truthy }
+        end
+      end
     end
   end
 end
