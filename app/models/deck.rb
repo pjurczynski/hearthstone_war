4924a4d53b22adab2314517146d104c9ae852eee
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-12 15:53:17 +0200

    if deck is removed, remove cards also

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,3 @@
 class Deck < ActiveRecord::Base
-  has_many :cards
+  has_many :cards, dependent: :destroy
 end
--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 describe Deck do
-  it { is_expected.to have_many :cards }
+  it { is_expected.to have_many(:cards).dependent(:destroy) }
 end
