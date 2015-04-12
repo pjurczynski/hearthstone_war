de98f8b66c1d0c926954c55f302c86fe37f1b6f3
Piotr Jurczynski <piotr.jurczynski@netguru.co>
2015-04-12 15:51:43 +0200

    card can deal damage

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
+
+  def deal_damage(card)
+    card.health -= attack
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 describe Card do
   it { is_expected.to belong_to :deck }
+
+  describe "#deal_damage" do
+    subject { your_card }
+    let(:your_card) { build :card }
+    let(:opponents_card) { build :card }
+
+    it 'hurts opponents card' do
+      expect{ subject.deal_damage(opponents_card) }
+        .to change{ opponents_card.health }.by(-subject.attack)
+    end
+  end
 end
